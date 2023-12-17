207b3ad44b07dcf690fd847961715122f229ae04
Arun Oli <arunoli555@gmail.com>
2023-12-17 07:45:54 +0545

    final update

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,13 +1,27 @@
 from django.contrib import admin
 from accounts.models import UserModel
+from django.utils.html import format_html
 from accounts import models
 
 class UserAdmin(admin.ModelAdmin):
     pass
 admin.site.register(UserModel, UserAdmin)
-# admin.site.register(models.DriverModel)
 admin.site.register(models.Verification)
-# admin.site.register(models.ClientModel)
 admin.site.register(models.RideModel)
 admin.site.register(models.ContactUsModel)
 admin.site.register(models.Rent)
+
+class DriverVerificationAdmin(admin.ModelAdmin):
+    list_display = ('user', 'display_photo', 'display_license', 'is_verified', )
+
+    def display_photo(self, obj):
+        return format_html('<img src="{}" width="70%" />', obj.photo.url)
+
+    display_photo.short_description = 'Photo'
+
+    def display_license(self, obj):
+        return format_html('<img src="{}" width="70%" />', obj.license.url)
+
+    display_license.short_description = 'License'
+
+admin.site.register(models.DriverVerification, DriverVerificationAdmin)
--- accounts/form.py
+++ accounts/form.py
@@ -1,15 +1,5 @@
 from django.forms import ModelForm
-from .models import Verification, RideModel, ContactUsModel
-
-# class DriverForm(ModelForm):
-#     class Meta:
-#         model = DriverModel
-#         fields = ['location', 'destination', 'date', 'time', 'seats', 'rideType']
-
-# class ClientForm(ModelForm):
-#     class Meta:
-#         model = ClientModel
-#         fields =['location', 'destination', 'date', 'time', 'seats', 'rideType', 'passenger', 'rent']
+from .models import Verification, RideModel, ContactUsModel, Rent, DriverVerification
 
 class VerificationForm(ModelForm):
     class Meta:
@@ -30,3 +20,14 @@ class ContactUsForm(ModelForm):
     class Meta:
         model = ContactUsModel
         fields = ['name', 'email', 'message']
+
+class RentForm(ModelForm):
+    class Meta:
+        model = Rent
+        fields = ['requestAccept']
+
+
+class DriverVerificationForm(ModelForm):
+    class Meta:
+        model = DriverVerification
+        fields = ["photo", "license"]
